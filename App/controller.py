3424d3483fa7c28ee5bff9689b5efe8d74846131
Paolaaaaaa <78177442+Paolaaaaaa@users.noreply.github.com>
2021-02-14 01:05:53 -0500

    funciones de inicialización y carga de datos

--- App/controller.py
+++ App/controller.py
@@ -30,8 +30,43 @@ El controlador se encarga de mediar entre la vista y el modelo.
 """
 
 # Inicialización del Catálogo de libros
+def initCatalog():
+    """
+    Llama la funcion de inicializacion del catalogo del modelo.
+    """
+    catalog = model.newCatalog()
+    return catalog
 
 # Funciones para la carga de datos
+def loadData(catalog):
+    """
+    Carga los datos de los archivos y cargar los datos en la
+    estructura de datos
+    """
+    loadVideos(catalog)
+    loadCategory_id(catalog):
+
+
+def loadVideos(catalog):
+    """
+    Carga los libros del archivo.  Por cada libro se toman sus autores y por
+    cada uno de ellos, se crea en la lista de autores, a dicho autor y una
+    referencia al libro que se esta procesando.
+    """
+    videosfile = cf.data_dir + 'Videos/videos-small.csv'
+    input_file = csv.DictReader(open(videosfile, encoding='utf-8'))
+    for video in input_file:
+        model.addVideo(catalog, video)
+
+
+def loadCategory_id(catalog):
+    """
+    Carga todos los tags del archivo y los agrega a la lista de tags
+    """
+    tagsfile = cf.data_dir + 'Videos/category-id.csv'
+    input_file = csv.DictReader(open(tagsfile, encoding='utf-8'), delimiter='\t'))
+    for tag in input_file:
+        model.addTag(catalog, tag)
 
 # Funciones de ordenamiento
 
